9f33edcbc4d3d140e85ecd00b3b8efba728cf166
Nikolett Muller <nm225@njit.edu>
2021-04-20 18:21:50 -0400

    Fix: changed title

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Movies Project'}
+    user = {'username': 'Robert De Niro'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM tableDeNiroMovies')
     result = cursor.fetchall()
