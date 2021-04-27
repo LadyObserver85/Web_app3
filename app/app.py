cceb3be81392deca84043d920da2bfafaf8c0df4
Nikolett Muller <nm225@njit.edu>
2021-04-26 22:00:47 -0400

    Task: updated api_add, api_edit and api_delete in app.py

--- app/app.py
+++ app/app.py
@@ -97,19 +97,35 @@ def api_retrieve(movie_id) -> str:
 
 @app.route('/api/v1/movies/', methods=['POST'])
 def api_add() -> str:
+    content = request.json
+    cursor = mysql.get_db().cursor()
+    inputData = (content['Year'], content['Score'], content['Title'])
+    sql_insert_query = """INSERT INTO tableDeNiroMovies (Year,Score,Title) VALUES (%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
     resp = Response(status=201, mimetype='application/json')
     return resp
 
 
 @app.route('/api/v1/movies/<int:movie_id>', methods=['PUT'])
 def api_edit(movie_id) -> str:
-    resp = Response(status=201, mimetype='application/json')
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['Year'], content['Score'], content['Title'], movie_id)
+    sql_update_query = """UPDATE tableDeNiroMovies t SET t.Year = %s, t.Score = %s, t.Title = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
     return resp
 
 
 @app.route('/api/movies/<int:movie_id>', methods=['DELETE'])
 def api_delete(movie_id) -> str:
-    resp = Response(status=210, mimetype='application/json')
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM tableDeNiroMovies WHERE id = %s """
+    cursor.execute(sql_delete_query, movie_id)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
     return resp
 
 
